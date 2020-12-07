8bea6a3704fc0c1053b4ae6989b4a18598fe0bd6
tachyon <83tachyon@gmail.com>
2020-12-07 14:42:09 +0900

    maybe complete?

--- models/PoolDAO.js
+++ models/PoolDAO.js
@@ -1,4 +1,7 @@
+const mysql = require('mysql');
 const dbCreate = require('./dbPoolCreator')
+const paginationSettings = require('../config/paginationSettings')
+const sqls = require('./settings/sqlDispenser')
 
 class PoolDao {
     constructor() {
@@ -7,6 +10,31 @@ class PoolDao {
             console.log('poolDao class instance and its dbPool created...')
         })
         // this.usedIds=[]
+    }
+
+    // arrow function is needed to have an access to this.dbpool
+    // in class, written in 'strict mode'
+
+    sqlHandler = (sql, q, fn) => {
+        if (q) sql = mysql.format(sql, q)
+        return new Promise((resolve, reject) => {
+            this.dbpool.getConnection((err, conn) => {
+                if (err) {
+                    if (conn) conn.release();
+                    reject(err)
+                    return;
+                }
+                conn.query(sql, (err, rows, fields) => {
+                    conn.release();
+                    if (err) {
+                        reject(err)
+                        return;
+                    }
+                    console.log('db process result', rows)
+                    resolve(rows)
+                })
+            })
+        })
     }
 
     create = (q, fn) => {
@@ -20,32 +48,11 @@ class PoolDao {
             q.poolOption,
             q.poolName,
         ]
-        let sql_create = 'insert into pooltable(poolName,poolAddress,poolPhone,poolTypeMask,poolOpentime,poolOption) select * from (select ? as poolName,? as poolAddress,? as poolPhone,? as poolTypeMask,? as poolOpentime,? as poolOption) as tmp where not exists(select poolName from pooltable where poolName = ?) limit 1;'
-        let sqlHandler = () => {
-            return new Promise((resolve, reject) => {
-                this.dbpool.getConnection((err, conn) => {
-                    if (err) {
-                        if (conn) conn.release();
-                        console.log('ERR: getConnection in sqlHandler');
-                        fn(err, null);
-                        return;
-                    }
-                    conn.query(sql_create, info, (err, rows, fields) => {
-                        conn.release();
-                        if (err) {
-                            reject(err);
-                            fn(err, null);
-                            return;
-                        }
-                        resolve({ response: true });
-                    })
-                })
-            })
-        }
-        async function resultSender() {
-            fn(null, await sqlHandler())
-        }
-        resultSender();
+        this.sqlHandler(sqls.sql_create, info, fn).then(res => {
+            fn(null, { response: true })
+        }).catch(err => {
+            fn(err, null)
+        })
     }
 
     update = (q, fn) => {
@@ -58,83 +65,22 @@ class PoolDao {
             q.poolOption,
             q.poolId,
         ]
-        let sql_update = 'update pooltable set poolName=?,poolAddress=?,poolPhone=?,poolTypeMask=?,poolOpentime=?,poolOption=? where poolId = ?;'
-        let sqlHandler = () => {
-            return new Promise((resolve, reject) => {
-                this.dbpool.getConnection((err, conn) => {
-                    if (err) {
-                        if (conn) conn.release();
-                        console.log('ERR: getConnection in sqlHandler');
-                        fn(err, null);
-                        return;
-                    }
-                    conn.query(sql_update, info, (err, rows, fields) => {
-                        conn.release();
-                        if (err) {
-                            reject(err);
-                            fn(err, null);
-                            return;
-                        }
-                        resolve({ response: true });
-                    })
-                })
-            })
-        }
-        async function resultSender() {
-            fn(null, await sqlHandler())
-        }
-        resultSender();
+        this.sqlHandler(sqls.sql_update, info, fn).then(res => {
+            fn(null, { response: true })
+        }).catch(err => {
+            fn(err, null)
+        })
     }
 
     delete = (id, fn) => {
-        let sql_delete = 'delete from pooltable where poolId = ?;'
-        let sqlHandler = () => {
-            return new Promise((resolve, reject) => {
-                this.dbpool.getConnection((err, conn) => {
-                    if (err) {
-                        if (conn) conn.release();
-                        console.log('ERR: getConnection in sqlHandler');
-                        fn(err, null);
-                        return;
-                    }
-                    conn.query(sql_delete, id, (err, rows, fields) => {
-                        conn.release();
-                        if (err) {
-                            reject(err);
-                            fn(err, null);
-                            return;
-                        }
-                        resolve({ response: true });
-                    })
-                })
-            })
-        }
-        async function resultSender() {
-            fn(null, await sqlHandler())
-        }
-        resultSender();
+        this.sqlHandler(sqls.sql_delete, id, fn).then(res => {
+            fn(null, { response: true })
+        }).catch(err => {
+            fn(err, null)
+        })
     }
 
     showAdminBoard = fn => {
-        let sql_adminBoard = 'select count(*) from pooltable'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 16&poolTypeMask=16'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 8&poolTypeMask=8'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 4&poolTypeMask=4'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 2&poolTypeMask=2'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 1&poolTypeMask=1'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 4&poolOption=4'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 2&poolOption=2'
-        sql_adminBoard += ' union '
-        sql_adminBoard += 'select count(*) from pooltable where 1&poolOption=1'
-        sql_adminBoard += ';'
-
         let boardItemNames = [
             'poolCount',
             'publicCount',
@@ -146,74 +92,24 @@ class PoolDao {
             'womanCount',
             'disabledCount',
         ]
-
-        let sqlHandler = () => {
-            return new Promise((resolve, reject) => {
-                this.dbpool.getConnection((err, conn) => {
-                    if (err) {
-                        if (conn) conn.release();
-                        console.log('ERR: getConnection in sqlHandler');
-                        fn(err, null);
-                        return;
-                    }
-                    conn.query(sql_adminBoard, (err, rows, fields) => {
-                        conn.release();
-                        if (err) {
-                            reject(err);
-                            fn(err, null);
-                            return;
-                        }
-                        let ret = {}
-                        for (let i in rows) ret[boardItemNames[i]] = Object.values(rows[i])[0]
-                        resolve(ret);
-                    })
-                })
-            })
-        }
-        async function resultSender() {
-            fn(null, await sqlHandler())
-        }
-        resultSender();
+        this.sqlHandler(sqls.sql_adminBoard, null, fn).then(rows => {
+            let ret = {}
+            for (let i in rows) ret[boardItemNames[i]] = Object.values(rows[i])[0]
+            fn(null, ret)
+        }).catch(err => {
+            fn(err, null)
+        })
     }
 
-    // arrow function is needed to have an access to this.dbpool
     findDetailById = (id, fn) => {
-        let sqlHandler = () => {
-            return new Promise((resolve, reject) => {
-                this.dbpool.getConnection((err, conn) => {
-                    if (err) {
-                        if (conn) conn.release();
-                        console.log('ERR: getConnection in sqlHandler');
-                        fn(err, null);
-                        return;
-                    }
-                    let sql_detail = 'select * from pooltable where poolId=?;'
-                    conn.query(sql_detail, id, (err, rows, fields) => {
-                        conn.release();
-                        if (err) {
-                            reject(err);
-                            fn(err, null);
-                            return;
-                        }
-                        resolve(rows[0])
-                    })
-                })
-            })
-        }
-        async function resultSender() {
-            fn(null, await sqlHandler())
-        }
-        resultSender();
+        this.sqlHandler(sqls.sql_detail, id, fn).then(res => {
+            fn(null, res[0])
+        }).catch(err => {
+            fn(err, null)
+        })
     }
-
-    // itemsPerPage등을 세팅 파일로 몰아주기
-    // sqlHandler-resultSender를 then으로 바꿔주기
-    // 함수에 넣어주는 쿼리문과 [?,?]만 달리하여 같은 함수 호출하게 설계하기
-    // req.query에서 [?,?]만들어주는 함수 분리하기
 
     findList = (q, fn) => {
-        let checked = '1';
-        let itemsPerPage = 4;
         let pageNumber = q.pageNumber;
         let searchWord = '%' + q.searchWord + '%';
 
@@ -225,9 +121,8 @@ class PoolDao {
             q.poolPrivate,
             q.poolPublic,
         ];
-
         let poolTypeMask = 0;
-        for (let i in poolTypeArray) if (poolTypeArray[i] == checked) poolTypeMask |= (1 << i);
+        for (let i in poolTypeArray) if (poolTypeArray[i] == paginationSettings.checked) poolTypeMask |= (1 << i);
 
         // opentime은 AND연산 후에 search opentime과 같으면 true
         let poolOpentime = q.poolOpentime;
@@ -240,56 +135,27 @@ class PoolDao {
         ]
         let poolOption = 0
         for (let i in poolOptionArray) if (poolOptionArray[i] == checked) poolOption |= (1 << i);
-        // let poolOption = q.poolForChild + q.poolForWoman + q.poolForDisabled;
 
-        var sql_select_totalCount = "select count(*) as cnt from pooltable where (poolName like ? or poolAddress like ?) and (poolTypeMask&?)=poolTypeMask and (poolOpentime&?)=? and (poolOption&?)=?;"
-        var sql_select = "select * from pooltable where (poolName like ? or poolAddress like ?) and (poolTypeMask&?)=poolTypeMask and (poolOpentime&?)=? and (poolOption&?)=? order by poolId limit ?,?;"
-        let sqlHandler = () => {
-            return new Promise((resolve, reject) => {
-                this.dbpool.getConnection((err, conn) => {
-                    if (err) {
-                        if (conn) conn.release();
-                        console.log('ERR: getConnection in sqlHandler');
-                        fn(err, null);
-                        return;
-                    }
-                    conn.query(sql_select_totalCount, [searchWord, searchWord, poolTypeMask, poolOpentime, poolOpentime, poolOption, poolOption], (error, rows, fields) => {
-                        if (err) {
-                            if (conn) conn.release();
-                            reject(err);
-                            fn(err, null);
-                            return;
-                        }
-                        let ret = [];
-                        ret.push(rows[0].cnt);
-                        // conn.query(sql_select, [searchWord, searchWord, poolTypeMask, poolOpentime, poolOpentime, poolOption, (pageNumber - 1) * itemsPerPage, (pageNumber - 1) * itemsPerPage + itemsPerPage], (error, rows, fields) => {
-                        conn.query(sql_select, [searchWord, searchWord, poolTypeMask, poolOpentime, poolOpentime, poolOption, poolOption, (pageNumber - 1) * itemsPerPage, itemsPerPage], (error, rows, fields) => {
-                            conn.release();
-                            if (err) {
-                                reject(err);
-                                fn(err, null);
-                                return;
-                            }
-                            let pools = []
-                            for (let row of rows) {
-                                pools.push(row)
-                            }
-                            ret.push(pools);
-                            resolve(ret);
-                        })
-                    })
-                })
+        let totalCountParams = [searchWord, searchWord, poolTypeMask, poolOpentime, poolOpentime, poolOption, poolOption]
+        let selectParams = [searchWord, searchWord, poolTypeMask, poolOpentime, poolOpentime, poolOption, poolOption, (pageNumber - 1) * paginationSettings.itemsPerPage, paginationSettings.itemsPerPage]
+
+        this.sqlHandler(sqls.sql_select_totalCount, totalCountParams, fn).then(cntRes => {
+            this.sqlHandler(sqls.sql_select, selectParams, fn).then(rows => {
+                let pools = []
+                for (let row of rows) {
+                    pools.push(row)
+                }
+                let result = {
+                    'totalCount': cntRes[0].cnt,
+                    'pools': pools,
+                }
+                fn(null, result)
+            }).catch(err => {
+                fn(err, null)
             })
-        }
-        async function resultSender() {
-            let ret = await sqlHandler();
-            let result = {
-                'totalCount': ret[0],
-                'pools': ret[1],
-            }
-            fn(null, result)
-        }
-        resultSender();
+        }).catch(err => {
+            fn(err, null)
+        })
     }
 }
 
