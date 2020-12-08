cbe226f4fd36257f39c1da1d765be79000425b82
tachyon <83tachyon@gmail.com>
2020-12-09 01:28:44 +0900

    cors origin

--- config/corsConfigurations.js
+++ config/corsConfigurations.js
@@ -1,6 +1,8 @@
 module.exports = {
-    origin_https: 'https://otters-pool.herokuapp.com',
-    origin_http: 'http://otters-pool.herokuapp.com',
+    // origin_https: 'https://otters-pool.herokuapp.com',
+    // origin_http: 'http://otters-pool.herokuapp.com',
     // origin: true,
-    // credentials: true,
+    origin: 'https://otters-pool.herokuapp.com',
+    credentials: true,
+    preflightContinue: true,
 }
