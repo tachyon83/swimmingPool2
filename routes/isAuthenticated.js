e8111d569048bbc44e6d4734a38c0fd4fda9cb2c
tachyon <83tachyon@gmail.com>
2020-12-09 01:57:10 +0900

    res.header

--- routes/isAuthenticated.js
+++ routes/isAuthenticated.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 router.route('/').get((req, res) => {
     console.log('path: /isAuthenticated, prior to login or admin')
+    res.header('Access-Control-Allow-Credentials', 'true');
     if (req.user) res.json({ response: true })
     else res.json({ response: false })
 })
