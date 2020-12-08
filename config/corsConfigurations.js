7e6dcf75819c5bacf657a6f42ea179d26cdc178b
tachyon <83tachyon@gmail.com>
2020-12-08 22:03:45 +0900

    cors settings added

--- config/corsConfigurations.js
+++ config/corsConfigurations.js
@@ -1,4 +1,6 @@
 module.exports = {
-    origin_https: 'https://otters-pool.herokuapp.com',
-    origin_http: 'http://otters-pool.herokuapp.com',
+    // origin_https: 'https://otters-pool.herokuapp.com',
+    // origin_http: 'http://otters-pool.herokuapp.com',
+    origin: true,
+    credentials: true,
 }
